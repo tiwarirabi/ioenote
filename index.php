159ae31823bae0b57fb533d46d5d7a6b73e726fc
tiwarirabi <ravbi2007@gmail.com>
2016-04-11 17:09:15 +0545

    R

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ioe Note</title>
+	<link rel="stylesheet" href="site_files/css/material.min.css">
+	<script src="site_files/js/material.min.js"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<!-- Uses a transparent header that draws on top of the layout's background -->
+<style>
+.inner {
+	margin:20px auto;
+	width:87%;
+
+}
+.demo-layout-transparent {
+  background: url('site_files/images/header.jpg') center / cover;
+}
+.demo-layout-transparent .mdl-layout__header,
+.demo-layout-transparent .mdl-layout__drawer-button {
+  /* This background is dark, so we set text to white. Use 87% black instead if
+     your background is light. */
+  color: white;
+}
+.demo-card-square.mdl-card {
+  width: 320px;
+  height: 320px;
+  float:left;
+  margin:30px;
+}
+.demo-card-square > .mdl-card__title {
+  color: #fff;
+  background:url('site_files/images/header.jpg') bottom right 15% no-repeat #46B6AC;
+}
+</style>
+</head>
+<body>
+
+
+<div class="demo-layout-transparent mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header mdl-layout__header--transparent ">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">IoeNotes</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <!-- Navigation -->
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="">BCT</a>
+        <a class="mdl-navigation__link" href="">BCE</a>
+        <a class="mdl-navigation__link" href="">BEL</a>
+        <a class="mdl-navigation__link" href="">BEX</a>
+      </nav>
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">IoeNotes</span>
+    <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="">BCT</a>
+        <a class="mdl-navigation__link" href="">BCE</a>
+        <a class="mdl-navigation__link" href="">BEL</a>
+        <a class="mdl-navigation__link" href="">BEX</a>
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+  	<div class="inner">
+  		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+		  <div class="mdl-card__title mdl-card--expand">
+		    <h2 class="mdl-card__title-text">Update</h2>
+		  </div>
+		  <div class="mdl-card__supporting-text">
+		    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		    Aenan convallis.
+		  </div>
+		  <div class="mdl-card__actions mdl-card--border">
+		    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+		      View Updates
+		    </a>
+		  </div>
+		</div>
+
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+		  <div class="mdl-card__title mdl-card--expand">
+		    <h2 class="mdl-card__title-text">Update</h2>
+		  </div>
+		  <div class="mdl-card__supporting-text">
+		    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		    Aenan convallis.
+		  </div>
+		  <div class="mdl-card__actions mdl-card--border">
+		    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+		      View Updates
+		    </a>
+		  </div>
+		</div>
+<table class="mdl-data-table mdl-js-data-table  mdl-shadow--2dp">
+  <thead>
+    <tr>
+      <th class="mdl-data-table__cell--non-numeric">Material</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Acrylic (Transparent)</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Plywood (Birch)</td>
+    </tr>
+    <tr>
+      <td class="mdl-data-table__cell--non-numeric">Laminate (Gold on Blue) This is a good</td>
+    </tr>
+  </tbody>
+</table>
+
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+		  <div class="mdl-card__title mdl-card--expand">
+		    <h2 class="mdl-card__title-text">Update</h2>
+		  </div>
+		  <div class="mdl-card__supporting-text">
+		    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		    Aenan convallis.
+		  </div>
+		  <div class="mdl-card__actions mdl-card--border">
+		    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+		      View Updates
+		    </a>
+		  </div>
+		</div>
+
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+		  <div class="mdl-card__title mdl-card--expand">
+		    <h2 class="mdl-card__title-text">Update</h2>
+		  </div>
+		  <div class="mdl-card__supporting-text">
+		    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		    Aenan convallis.
+		  </div>
+		  <div class="mdl-card__actions mdl-card--border">
+		    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+		      View Updates
+		    </a>
+		  </div>
+		</div>
+
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+		  <div class="mdl-card__title mdl-card--expand">
+		    <h2 class="mdl-card__title-text">Update</h2>
+		  </div>
+		  <div class="mdl-card__supporting-text">
+		    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		    Aenan convallis.
+		  </div>
+		  <div class="mdl-card__actions mdl-card--border">
+		    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+		      View Updates
+		    </a>
+		  </div>
+		</div>
+
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+		  <div class="mdl-card__title mdl-card--expand">
+		    <h2 class="mdl-card__title-text">Update</h2>
+		  </div>
+		  <div class="mdl-card__supporting-text">
+		    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		    Aenan convallis.
+		  </div>
+		  <div class="mdl-card__actions mdl-card--border">
+		    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+		      View Updates
+		    </a>
+		  </div>
+		</div>
+	</div><!-- inner -->
+  </main>
+</div>
+
+
+
+</body>
+</html>
